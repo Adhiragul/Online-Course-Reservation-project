972222f823c79066517611ec038b9817af236662
ADHI RAGUL R <adhiragul7307@gmail.com>
2026-02-21 23:51:51 +0530

    Clean up comments in Main.java
    Removed comments regarding system look and feel and GUI launch.

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,13 +7,11 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        // Use the system look and feel for native appearance
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ignored) {
         }
 
-        // Launch the GUI on the Event Dispatch Thread
         SwingUtilities.invokeLater(MainFrame::new);
     }
 }
